7716f770ad9d7769f2d370021dae92ee8fc9a3de
namwaya <duncan.owino@gmail.com>
2024-05-22 07:12:21 +0100

    hello

--- 1-chap1-start-intro.js
+++ 1-chap1-start-intro.js
@@ -1,3 +1,7 @@
 //This line will print 'hello world' to the console.
 console.log('hello world');
 
+//This line will print the sum of 4 and 10 to the console.
+console.log(4 + 10)
+
+//Other js methods are console.log(), console.warn(), console.error(), console.clear(), console.time
