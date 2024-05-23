84cd80a164853fa80ba090aa2ea6e8ac753318f2
namwaya <duncan.owino@gmail.com>
2024-05-23 10:17:29 +0100

    indentation and white space

--- 4-chap1-indentation-whitespace.js
+++ 4-chap1-indentation-whitespace.js
@@ -1,9 +1,15 @@
+/* When you are writing code, often a line of code belongs to a certain code block (code
+between two curly brackets { like this }) or parent statement. If that is the case,
+you give the code in that block one indentation to make sure that you can see easily
+what is part of the block and when a new block starts. */
+
+
 let status="new";
 let scared= true;
 
 if (status === "new") {
     console.log("Welcome to Javascript!");
-    if scared==true {
+    if (scared) {
         console.log("Don't worry you will be fine!");
     } else {
         console.log("You are brave! You are going to do great things!");
