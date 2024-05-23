1c5a72bbabcb1ec651f08dea1de1563fffcbabfd
namwaya <duncan.owino@gmail.com>
2024-05-23 10:11:56 +0100

    indentation and white space

---
+++ 4-chap1-indentation-whitespace.js
@@ -0,0 +1,13 @@
+let status="new";
+let scared= true;
+
+if (status === "new") {
+    console.log("Welcome to Javascript!");
+    if scared==true {
+        console.log("Don't worry you will be fine!");
+    } else {
+        console.log("You are brave! You are going to do great things!");
+    }
+} else {
+    console.log("Welcome back, I knew you'd like it!");
+}
