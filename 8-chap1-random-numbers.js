3c836abb003a26b97732a06f0c11e6bb140110ce
namwaya <duncan.owino@gmail.com>
2024-05-24 08:46:31 +0100

    random

---
+++ 8-chap1-random-numbers.js
@@ -0,0 +1,9 @@
+//random number generator we will use Math.random() function in javascript.
+
+console.log(Math.random()); //prints a random number between 0 and 1
+
+console.log(Math.floor(Math.random() * 10)); //prints a random number between 0 and 9
+
+console.log(Math.ceil(Math.random() * 10)); //prints a random number between 1 and 10
+
+console.log(Math.round(Math.random() * 10)); //prints a random number between 0 and 10
