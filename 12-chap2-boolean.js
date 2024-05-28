f94387a2f81b9cf103270a6fd6ec42d1a95643a1
Duncan <duncan.owino@gmail.com>
2024-05-28 18:09:26 +0100

    boolean

---
+++ 12-chap2-boolean.js
@@ -0,0 +1,7 @@
+//boolean is a data type that can only be true or false.
+
+let objectIsDeleted = false;
+let lightIsOn = true;
+
+console.log(objectIsDeleted); //This will print false
+console.log(lightIsOn); //This will print true
