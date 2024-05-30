73514a9334499680aea2b9d57654a4d17eb8ee60
Duncan <duncan.owino@gmail.com>
2024-05-30 07:55:46 +0100

    undefined

---
+++ 14-chap2-undefined.js
@@ -0,0 +1,6 @@
+
+//JavaScript is a very special language. It has a special data type for a variable that has not been assigned a value. And this data type is undefined:
+let unassigned;
+
+console.log(unassigned); //This will print 'undefined'.
+
