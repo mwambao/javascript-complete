b76b2eeba833b836eb90b91e736563c096f8b428
Duncan <duncan.owino@gmail.com>
2024-06-11 21:52:01 +0100

    multiply

---
+++ 21-chap2-operators-subtraction.js
@@ -0,0 +1,11 @@
+let num1 = 45;
+let num2 = 67;
+console.log(num1 - num2); // -22
+
+let str1 = "Duncan";
+let num3 = 34;
+console.log(str1 - num3); //NaN. Not an error - This is javascript way of not crashing as we trying to subtracts a number from a string - 'Not a Number'.
+
+
+
+  
---
+++ 22-chap2-operators-multiplication.js
@@ -0,0 +1,11 @@
+let nr1 = 15;
+let nr2 = 10;
+
+console.log(nr1 * nr2); // 150
+
+let str1 = "Hi";
+let nr3 = 23;
+
+console.log(str1 * nr3); //NaN
+
+
