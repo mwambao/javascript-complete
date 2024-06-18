1b08d0e86db781fbb19cdfb27afc297ed0743bd4
Duncan <duncan.owino@gmail.com>
2024-06-18 08:41:28 +0100

    greater-than-smaller-than

---
+++ 31-chap2-comparison-operator-greater-than-small-than.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 6;
+
+//greater than
+console.log(x > y); // false
+console.log(y > y); //false
+console.log(y >= x); //true
+console.log(y >= y); //true. Since it equal to itself
+
+
+//smaller than
+console.log(x < y); // true
+console.log(y < y); //false
+console.log(y <= y); //true. since it is equal to itself
