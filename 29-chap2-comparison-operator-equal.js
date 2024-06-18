ff29416124ca276793a8b66d0e19346f757e4bfc
Duncan <duncan.owino@gmail.com>
2024-06-18 08:31:30 +0100

    equal

---
+++ 29-chap2-comparison-operator-equal.js
@@ -0,0 +1,7 @@
+let x = 5;
+let y = '5';
+
+console.log(x == y); //prints true. Double equal '==' doesn't check data type.it only checks for equal value.
+
+console.log(x === y); //prints false. Triple equal '===' checks equal and data type. It evaluates both data type and value.
+
