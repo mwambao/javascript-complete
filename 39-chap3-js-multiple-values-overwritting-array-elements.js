89f13ad547aa1390bb13a597b531a2c38e2a67af
Duncan <duncan.owino@gmail.com>
2024-06-23 12:41:31 +0100

    overwritting array elements

--- 39-chap3-js-multiple-values-overwritting-array-elements.js
+++ 39-chap3-js-multiple-values-overwritting-array-elements.js
@@ -5,3 +5,9 @@ cars[0] = 'Audi';
 console.log(cars[0]); // Audi
 console.log(cars) // Prints [ 'Audi', 'Renault', 'Vokswagen' ]
 
+//
+cars[3] = 'Mazda';
+console.log(cars[3]); // Mazda
+
+cars[-1] = 'Octavia';
+console.log(cars[-1]); // Octavia
