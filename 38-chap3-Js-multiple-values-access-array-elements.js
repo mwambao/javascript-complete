903746911b975c0108c80a3cd81efd4fcb09d10f
Duncan <duncan.owino@gmail.com>
2024-06-23 12:25:03 +0100

    accessing arrays

---
+++ 38-chap3-Js-multiple-values-access-array-elements.js
@@ -0,0 +1,11 @@
+//How to access elements in an array in JS.
+
+cars = ['Toyota', 'Renault', 'Vokswagen'];
+
+console.log(cars[0]); //Toyota
+console.log(cars[1]); //Renault
+console.log(cars[2]); //Volkswagen
+
+//JS doesn't support non-existent or negative indexes.
+console.log(cars[3]); //undefined
+console.log(cars[-1]); //undefined.
