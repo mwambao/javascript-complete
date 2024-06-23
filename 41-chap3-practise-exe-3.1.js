acfb36fe777739cd448d33af24f095389708914a
Duncan <duncan.owino@gmail.com>
2024-06-23 13:21:56 +0100

    practise exe 3.1

---
+++ 41-chap3-practise-exe-3.1.js
@@ -0,0 +1,15 @@
+/*
+1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and
+"Apples."
+2. Check your list length in the console.
+3. Update "Bread" to "Bananas."
+4. Output your entire list to the console.
+*/
+
+shoppingList = ['Milk', 'Bread', 'Apples'];
+console.log(shoppingList) //[ 'Milk', 'Bread', 'Apples' ]
+
+console.log('The list length is: ' + shoppingList.length);
+
+shoppingList[1] = 'Bananas'; // update Bread to Bananas
+console.log(shoppingList) // [ 'Milk', 'Bananas', 'Apples' ]
