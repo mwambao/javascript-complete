e38ce92554fb0f7fd1a45173653c5b06dcf44ce3
Duncan <duncan.owino@gmail.com>
2024-06-27 10:26:29 +0100

    sorting

---
+++ 47-chap2-js-multiple-values-sorting-arrays.js
@@ -0,0 +1,13 @@
+/*
+sort() -> sorts elements A-Z and numbers from lowers to highest(ascending order)
+*/
+
+let ourNames = ['James', 'Alicia', 'Faith', 'Maria', 'Bert'];
+ourNames.sort();
+
+console.log(ourNames); //[ 'Alicia', 'Bert', 'Faith', 'James', 'Maria' 
+
+let myNums = [18, 72, 33, 56, 40];
+myNums.sort();
+
+console.log(myNums); //[ 18, 33, 40, 56, 72 ]
