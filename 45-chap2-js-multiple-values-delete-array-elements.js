e761083c9de17b0d107443daa455fb740fdf9b6a
Duncan <duncan.owino@gmail.com>
2024-06-27 09:59:26 +0100

    delete array elements

---
+++ 45-chap2-js-multiple-values-delete-array-elements.js
@@ -0,0 +1,18 @@
+/* 
+To delete:
+pop()
+shift()
+splice() <--- We already looked at this
+*/
+
+myArr = [1,2,3,4,5,6,7,8,9];
+
+myArr.pop(); //To delete the last element
+
+console.log(myArr); //[ 1, 2, 3, 4, 5, 6, 7, 8]
+
+myArr.shift(); //To delete the first element
+
+console.log(myArr); //[ 2, 3, 4, 5, 6, 7, 8]
+
+
