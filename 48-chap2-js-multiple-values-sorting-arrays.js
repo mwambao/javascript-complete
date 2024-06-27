b14cafba607142383bbee4387f8ee7152a1f444e
Duncan <duncan.owino@gmail.com>
2024-06-27 10:40:50 +0100

    reverse

---
+++ 48-chap2-js-multiple-values-sorting-arrays.js
@@ -0,0 +1,10 @@
+/*
+reverse()
+- It doesn't matter whethere the arrays is sorted or not, it just reverses the order of the elements
+*/
+
+let ourNames = ['James', 'Alicia', 'Faith', 'Maria', 'Bert'];
+
+ourNames.reverse();
+
+console.log(ourNames); // [ 'Bert', 'Maria', 'Faith', 'Alicia', 'James' ]
