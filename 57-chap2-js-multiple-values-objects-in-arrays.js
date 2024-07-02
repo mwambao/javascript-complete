1b7eaece3713482fa44deaa0d20707c1cca070d9
Duncan <duncan.owino@gmail.com>
2024-07-02 19:26:51 +0100

    objects and arrays exe 3.5

---
+++ 57-chap2-js-multiple-values-objects-in-arrays.js
@@ -0,0 +1,20 @@
+//below we  objects {} inside an array []
+//just remember wheneve you need a list of something, use array. Whenever you have something with descriptive names use objects
+let addresses = [
+    {
+        street: '2nd street',
+        number: '123',
+        zipcode: '33316',
+        city: 'Miami',
+        stat: 'Florida'
+    },
+    {
+        street: '1st west avenue',
+        number: '5',
+        zipcode: '7656',
+        city: 'Adison',
+        stat: 'Texas'
+    }
+];
+
+console.log(addresses[1].city) //Adison
---
+++ 58-chap2-js-practice-exe-3.5-objects-and-arrays.js
@@ -0,0 +1,55 @@
+
+/*
+Practice exercise 3.5
+1. Create an object named people that contains an empty array that is called
+friends.
+2. Create three variables, each containing an object, that contain one of your
+friend's first names, last names, and an ID value.
+3. Add the three friends to the friend array.
+4. Output it to the console.
+*/
+
+//an object named people that contain an empty array called friends
+let people = {
+    friends:[]
+};
+
+console.log(people); //{ friends: [] }
+
+//1st variable
+variableX = {
+    firstName : 'Alex',
+    lastName: 'Macharia',
+    idValue: '2345'
+};
+
+//2nd variable
+variableY = {
+    firstName : 'Michael',
+    lastName: 'Owino',
+    idValue: '1301'
+}
+
+//3rd variable
+variableZ = {
+    firstName : 'Faith',
+    lastName: 'Ngao',
+    idValue: '7890'
+}
+
+people.friends[0] = variableX;
+people.friends[1] = variableY;
+people.friends[2] = variableZ;
+
+console.log(people);
+
+/*
+{
+  friends: [
+    { firstName: 'Alex', lastName: 'Macharia', idValue: '2345' },
+    { firstName: 'Michael', lastName: 'Owino', idValue: '1301' },
+    { firstName: 'Faith', lastName: 'Ngao', idValue: '7890' }
+  ]
+}
+*/
+
