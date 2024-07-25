c2230f8cba36786b4bb3b4ed69ea29c886b18a19
Duncan <duncan.owino@gmail.com>
2024-07-25 08:49:31 +0100

    loops and arrays

---
+++ 81-chap5-loops-loops-and-arrays.js
@@ -0,0 +1,23 @@
+/*
+
+let arr = [some array];
+
+for (initialize variable; variable smaller than arr.length; statement) {
+    //code to be executed
+}
+
+*/
+
+let names = ["Chantal", "John", "Bobbi", "Maxime", "Jair"];
+
+for (i=0; i < names.length ; i++) {
+    console.log(names[i]);
+}
+
+/*
+Chantal
+John
+Bobbi
+Maxime
+Jair
+*/
