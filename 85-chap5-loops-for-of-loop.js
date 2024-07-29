2255967446105067fd2c65a9ade0cb400e1f2047
Duncan <duncan.owino@gmail.com>
2024-07-29 08:32:17 +0100

    for of loop

---
+++ 85-chap5-loops-for-of-loop.js
@@ -0,0 +1,22 @@
+/*
+    let arr = [some array];
+
+    for (let variablename of arr) {
+    //code to be executed
+    //value of variablename gets updated each interation
+    //all values of the array will be variablename once}
+*/
+
+let names = ["Chantal", "John", "Maxime", "Bobbi", "Jair"];
+
+for (let name of names) {
+    console.log(name);
+}
+
+/*
+Chantal
+John
+Maxime
+Bobbi
+Jair
+*/
