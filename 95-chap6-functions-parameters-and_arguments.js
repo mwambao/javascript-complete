da9a4b2ae5a6801a101b2994c7dccaad977b4da6
Duncan <duncan.owino@gmail.com>
2024-09-25 10:31:27 +0100

    functions

---
+++ 95-chap6-functions-parameters-and_arguments.js
@@ -0,0 +1,10 @@
+
+//PARAMETER - A parameter is defined as a variable listed inside the parentheses of the function definiction, which defines the scope of the function.
+function tester(parameter1, parameter2) {
+    return parameter1 + " " + parameter2;
+}
+
+
+
+//ARGUMENT - When a function is called we simply add the arguments to it.
+console.log(tester("argument1", "argument2")); // argument1 argument2
