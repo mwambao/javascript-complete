d33c197401dcb08bc26537a90b9a910e160cb311
Duncan <duncan.owino@gmail.com>
2024-09-25 10:03:07 +0100

    functions

---
+++ 94-chap6-functions-exercise-6.1-basic-fuctions.js
@@ -0,0 +1,10 @@
+
+//a function taking two PARAMETERS and returning their sum
+function addNumbers(x, y) {
+return x + y;
+}
+
+//calling function with two ARGUMENTS
+console.log(addNumbers(6,7)); //  13
+
+console.log(addNumbers(123, 89)); // 212
