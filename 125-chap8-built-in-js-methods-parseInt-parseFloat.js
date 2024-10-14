7d298c0c71f6be3ee81c5337a7e0f81720e0f73e
Duncan <duncan.owino@gmail.com>
2024-10-14 20:18:28 +0100

    js built-in methods

--- 125-chap8-built-in-js-methods-parseInt-parseFloat.js
+++ 125-chap8-built-in-js-methods-parseInt-parseFloat.js
@@ -3,13 +3,21 @@ In many situations you will have
 to translate a string to a number, for example reading input boxes from an HTML web
 page. You cannot calculate with strings, but you can with numbers. Depending on
 what exactly you need to do, you will need either one of these methods.
-- parseInt parses a string to intenger. 
-- parseFloat whenever you have values or integres
+- parseInt() parses a string to intenger. 
+- parseFloat() whenever you have values or integers. You will work with this if you need decimal numbers.
 
 */
 
+//parseInt()
 
 let str_int = "6";
 let int_int = parseInt(str_int);
-
 console.log("Type of ", int_int, " is ", typeof int_int); // Type of  6  is  number
+
+
+//parseFloat()
+let str_float = "7.6";
+let float_float = parseFloat(str_float);
+console.log("Type of ", str_float , " is ", typeof float_float); // Type of  7.6  is  number
+
+
---
+++ 126-chap8-built-in-js-methods-forEach.js
@@ -0,0 +1,20 @@
+/*
+The forEach() method in JavaScript is used to execute a provided function once for each array element. 
+*/
+
+
+let myArray = ["Grapefruit", 4, "Hello", 5.6, true];
+
+function printStuff(element, index){
+    console.log("printing stuff:", element, "on array position", index);
+}
+
+//We just need to specify what function needs to be executed for every element.
+myArray.forEach(printStuff);
+
+/*
+printing stuff: 4 on array position 1
+printing stuff: Hello on array position 2
+printing stuff: 5.6 on array position 3
+printing stuff: true on array position 4
+*/
