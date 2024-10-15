dafac801a3f34117b9325df29e728785af75f2be
Duncan <duncan.owino@gmail.com>
2024-10-15 23:03:57 +0100

    js built-in methods

---
+++ 138-chap8-built-in-js-methods-strings-toUpperCase-toLowerCase.js
@@ -0,0 +1,10 @@
+/*
+toUpperCase -> change string to upper case
+toLowerCase -> change string to lower case
+*/
+
+let low_bye = "bye!";
+console.log(low_bye.toLocaleUpperCase()); // BYE!
+
+let caps = "HOW ARE YOU?";
+console.log(caps.toLocaleLowerCase()); // how are you?
