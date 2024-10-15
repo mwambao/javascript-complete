66dffcca92fbd8217c138adfb8df2342fb5822db
Duncan <duncan.owino@gmail.com>
2024-10-15 23:12:55 +0100

    js built-in methods

---
+++ 139-chap8-built-in-js-methods-strings-startsWith-endsWith.js
@@ -0,0 +1,10 @@
+
+//sometimes you want to check what a string starts or ends with.
+
+let encouragement = "You are doing great, keep up the good work";
+
+//true since the string beginds with 'You'
+console.log(encouragement.startsWith("You")); // true
+
+//false since the string doesnt end with 'good'
+console.log(encouragement.endsWith("good")); // false
