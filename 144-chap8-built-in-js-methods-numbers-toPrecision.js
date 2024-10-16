30b8382f0b2cd98e34247de57e37f6364e884268
Duncan <duncan.owino@gmail.com>
2024-10-16 21:37:05 +0100

    js built-in methods

---
+++ 144-chap8-built-in-js-methods-numbers-toPrecision.js
@@ -0,0 +1,7 @@
+/* toPrecision() - The toPrecision() method in JavaScript is used to format a number to a specific precision, meaning the total number of digits, including those before and after the decimal point.
+*/
+
+let x  = 1.23456;
+console.log(x.toPrecision(2)); //1.2
+
+
---
+++ 145-chap8-built-in-js-methods-math-max-min.js
@@ -0,0 +1,5 @@
+
+console.log(Math.max(2, 56, 12, 1, 233, 4)); // 233
+
+console.log(Math.min(2, 56, 12, 1, 233, 4)); // 1
+
