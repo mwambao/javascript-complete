72c21a00de5d655178826bcd971019ec9d791a89
Duncan <duncan.owino@gmail.com>
2024-10-17 22:30:04 +0100

    js built-in methods

---
+++ 148-chap8-built-in-js-methods-math-exp-log.js
@@ -0,0 +1,15 @@
+/*
+The exponent is the number to which a base is being raised. We use e (Euler's
+number) a lot in mathematics, this is what the exp() method in JavaScript does. It
+returns the number to which e must be raised to get the input. We can use the exp()
+built-in method of Math to calculate the exponent, and the log() method to calculate
+the natural logarithm.
+*/
+
+x = 2;
+let exp = Math.exp(x);
+console.log(exp); // 7.38905609893065
+
+let log = Math.log(exp);
+console.log(log); //2
+
