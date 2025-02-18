cfdd3c9f92a580f44d9ce2450e629e454adf5e03
Duncan Owino Mwamba <owino@mac.home>
2025-02-18 20:48:54 +0000

    concurency intro

---
+++ 194-chap13-concurency-intro.js
@@ -0,0 +1,10 @@
+/*
+* concurency -> happening at same time / in parallel. we can do an API call and not wait for the reply but do something useful in the meantime instead.
+            -> three statergies while working with concurency in JS: callbacks, promises and async and await
+
+i - callbacks -> A function taking another function as an argument.
+ii - promises -> A promise is a special object that connects code that needs to produce a result and the code that needs to use this result in the next step.
+iii - async / await -> async: we mak a function return a promise, await: to wait until the promise is done.
+
+* Event loop -> The event loop in JavaScript is like a waiter in a restaurant who takes orders, delivers food, and keeps checking if new tasks need to be handled.
+*/
